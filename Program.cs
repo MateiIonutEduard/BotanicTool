1186beaa80c90c108c1069b151d6f0c0c286b7f9
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-04-18 13:54:16 +0300

    Add help feature at this CLI tool
    Add help feature at this CLI tool.

--- Program.cs
+++ Program.cs
@@ -9,19 +9,44 @@ namespace BotanicTool
     {
         static async Task Main(string[] args)
         {
-            if(args.Length != 1)
+            string? help = args.FirstOrDefault(arg => arg == "-h" || arg == "--help");
+            string? path = args.FirstOrDefault(arg => arg == "-o" || arg == "--output");
+
+            string? folderPath = args.SkipWhile(arg => arg == "-o" || arg == "--output").FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(help))
+            {
+                if (string.IsNullOrEmpty(path))
+                {
+                    Console.WriteLine("Usage: BotanicTool [-o] <directory_path>");
+                    Console.WriteLine("\nOptions:");
+                    Console.WriteLine("  -o, --output  specify directory path");
+                    Console.WriteLine("  -h, --help  show this help message and exit");
+                    Environment.Exit(-1);
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(path))
+                {
+                    Console.WriteLine("Specify only destination folder path.");
+                    Environment.Exit(-1);
+                }
+            }
+
+            if(string.IsNullOrEmpty(folderPath))
             {
                 Console.WriteLine("Specify only destination folder path.");
                 Environment.Exit(-1);
             }
-
+            
             // save plants list at specified file
-            string plantsPath = Path.Combine(args[0], "plants.json");
+            string plantsPath = Path.Combine(folderPath, "plants.json");
             string data = await CoreUtil.GetPlantItems(plantsPath);
             await File.WriteAllTextAsync(plantsPath, data);
 
             // save sql file that contains products and categories
-            await CoreUtil.GetProductsAsync(args[0]);
+            await CoreUtil.GetProductsAsync(folderPath);
         }
     }
 }
